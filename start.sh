cce79c225e48d8d4e2ff0cc198afb4efc6045d05
Moe Adham <moeadham@gmail.com>
2014-06-12 01:15:33 -0700

    [startup] fix startup script

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 bitcoind -datadir='/data/bitcoin' -daemon -rpcallowip='127.0.0.1'
-sleep 60
-cd /opt/insight-api/
-INSIGHT_NETWORK=livenet BITCOIND_USER=user BITCOIND_PASS=pass INSIGHT_PUBLIC_PATH=public  npm start
+sleep 20
+service nginx start &
+INSIGHT_NETWORK=livenet BITCOIND_USER=user BITCOIND_PASS=pass INSIGHT_PUBLIC_PATH=/opt/insight/public INSIGHT_DB=/data/insight /usr/bin/forever start --sourceDir /opt/insight/ -l /var/log/forever.out.log -o /var/log/insight.out.log -e /var/log/insight.err.log
+echo "Started all.."
