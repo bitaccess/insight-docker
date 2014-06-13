9336ddf3a404ea8aa07924e2dd30611f5cb2b413
Moe Adham <moeadham@gmail.com>
2014-06-12 21:10:48 -0700

    [Docker] Add script to clean out docker

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 bitcoind -datadir='/data/bitcoin' -daemon -rpcallowip='127.0.0.1'
-sleep 20
+sleep 60
 service nginx start &
 cd /opt/insight
 INSIGHT_NETWORK=livenet BITCOIND_USER=user BITCOIND_PASS=pass INSIGHT_PUBLIC_PATH=public INSIGHT_DB=/data/insight /usr/bin/forever start --sourceDir /opt/insight/ -l /var/log/forever.out.log -o /var/log/insight.out.log -e /var/log/insight.err.log -a node_modules/insight-bitcore-api/insight.js
